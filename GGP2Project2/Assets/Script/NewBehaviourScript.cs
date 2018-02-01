7a3bc1c5e76b4e0f0ebdccb376fc799eb441d8a6
CATBC\pratttre <treazurep@gmail.com>
2018-01-31 21:02:22 -0500

    First

---
+++ GGP2Project2/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Rigidbody2D playerRigidBody2D; 
+    public float movePlayerHorizontal;
+    public Vector2 movement;
+
+    public float speed = 4.0f;
+
+    void Awake()
+    {
+        playerRigidBody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        movePlayerHorizontal = Input.GetAxis("Horizontal");
+        movement = new Vector2(movePlayerHorizontal, y: 0);
+
+        playerRigidBody2D.velocity = movement * speed;
+		
+	}
+}
